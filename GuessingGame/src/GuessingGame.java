6673ab7646fc2b085fdb6894a33ad2f0d63bfa69
Unc-Elman <emmanuelokemwa3@gmail.com>
2026-08-17 15:55:05 +0300

    Finished guessing game code

--- GuessingGame/src/GuessingGame.java
+++ GuessingGame/src/GuessingGame.java
@@ -8,11 +8,13 @@ public class GuessingGame {
 
         int numberGuess = random.nextInt(10) + 1;
         int userGuess = 0;
+        int guessCount = 0;
 
         System.out.print("Guess a number between 1 and 10:");
 
         while (userGuess != numberGuess) {
             userGuess = scanner.nextInt();
+            guessCount++;
 
             if (userGuess < numberGuess) {
                 System.out.println("Higher number. Try again.");
@@ -20,6 +22,7 @@ public class GuessingGame {
                 System.out.println("Lower number. Try again.");
             } else {
                 System.out.println("You found the number!");
+                System.out.println("It took you " + guessCount + " guesses.");
             }
         }
         scanner.close();
